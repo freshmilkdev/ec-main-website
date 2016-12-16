d8b00edb6237a18fde69d731bc25f05e028a87fe
Max <max.ruchkin@gmail.com>
2016-12-16 17:10:32 +0200

    material form

--- src/index.js
+++ src/index.js
@@ -8,3 +8,21 @@ import "./services.html";
 import materialDesignHamburger from './assets/js/menu-hamburger';
 
 materialDesignHamburger();
+
+var textarea = document.querySelector('#contact-form textarea');
+textarea.onkeyup = function (e) {
+   if(e.target.value.trim().length > 0){
+       e.target.classList.add('valid');
+   }else{
+       e.target.classList.remove('valid');
+   }
+};
+
+var select = document.querySelector('#contact-form select');
+select.onchange = function (e) {
+    if(e.target.value != 0){
+        e.target.classList.add('valid');
+    }else{
+        e.target.classList.remove('valid');
+    }
+};
